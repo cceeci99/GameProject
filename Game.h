edbbaca46241864c692e79f54ec6a0169e519d8f
dimitrskpl <dimitrisatromitara@gmail.com>
2021-01-04 17:20:29 +0200

    ΑΑΑ

--- Game.h
+++ Game.h
@@ -2,6 +2,7 @@
 #include "Map/Grid/Grid.h"
 #include "MarketPlace/Market.h"
 #include "Creatures/Monsters/Monster.h"
+#include "Creatures/Heroes/Hero.h"
 
 #ifndef GAMEPROJECT_GAME_H
 #define GAMEPROJECT_GAME_H
@@ -78,6 +79,57 @@ public:
         std::cout << "Whenever you get involved in fight and it's your turn you can display stats of your team and enemies by pressing (t)" << std::endl;
         std::cout << "You can make normal attack by pressing (o), cast spell by pressing (l) or use potion by pressing (p)" << std::endl;
 
+    }
+
+    void figth(HeroSquad ){
+        cout << "Fight..." << endl;
+
+        int totalMonsters = rand() % 5 + 1; //[1, 5]
+        //create MosterSquad
+        unsigned int j = 0;
+        for(int i = 0; i < totalMonsters; i++){
+            unsigned int curLevel = HeroSquad[j]->getLevel(); 
+            int MonsterType = rand % 3;
+            
+            switch (MonsterType)
+            {
+            case Dragon:
+                //...
+                break;
+            case ExoSkeleton:
+                //...
+                break;
+            }
+            case Spirit:
+                //...
+                break;
+            }
+
+            j++;
+            if(j >= sq->getSize())
+                j = 0;
+        }
+
+        unsigned int round = 1;
+        while(true){
+           if (HeroSquad.wiped() || MonsterSquad.wiped(){
+                //...   
+                break;
+           }
+
+            if(round % 2 == 1){ //Heroes' turn
+                //...        
+            }
+            else{ //Monsters' turn
+                //...
+            }
+
+            //REGENERATION
+            HeroSquad.regeneration();
+            MonsterSquad.regeneration();
+
+            round++;
+        }
     }
 
     static void quit(){
